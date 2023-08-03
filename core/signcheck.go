b03a017e9f8f99d57dee7fa1983b4d5bfa8055d3
Mateus Melchiades <matbme@duck.com>
2023-08-03 09:44:55 -0300

    signcheck: Replace byte arrays with strings
    Makes things easier to read

--- core/signcheck.go
+++ core/signcheck.go
@@ -3,50 +3,58 @@ package core
 import (
 	"bytes"
 	"fmt"
-	"github.com/jedisct1/go-minisign"
 	"os"
 	"strings"
+
+	"github.com/jedisct1/go-minisign"
+)
+
+var (
+	signatureFileSeparator    = []byte("----begin attach----")
+	signatureFileEndSeparator = []byte("----begin second attach----")
+	signatureHashSeparator    = []byte("----begin second attach----")
 )
 
 func GetSignatureFile(binary string) (string, error) {
-	signatureFileSeparator := []byte{0x2D, 0x2D, 0x2D, 0x2D, 0x62, 0x65, 0x67, 0x69, 0x6E, 0x20, 0x61, 0x74, 0x74, 0x61, 0x63, 0x68, 0x2D, 0x2D, 0x2D, 0x2D}
-	signatureFileEndSeparator := []byte{0x2D, 0x2D, 0x2D, 0x2D, 0x62, 0x65, 0x67, 0x69, 0x6E, 0x20, 0x73, 0x65, 0x63, 0x6F, 0x6E, 0x64, 0x20, 0x61, 0x74, 0x74, 0x61, 0x63, 0x68, 0x2D, 0x2D, 0x2D, 0x2D}
-
 	data, err := os.ReadFile(binary)
 	if err != nil {
 		return "", err
 	}
+
 	signatureFileIndex := bytes.LastIndex(data, signatureFileSeparator) + len(signatureFileSeparator)
 	signatureFileEndIndex := bytes.LastIndex(data, signatureFileEndSeparator) + len(signatureFileEndSeparator)
 	signatureFile := ""
+
 	for i := 0; i < signatureFileEndIndex-signatureFileIndex-len(signatureFileEndSeparator); i++ {
 		signatureFile = signatureFile + string(data[signatureFileIndex+i])
 	}
+
 	if strings.TrimSpace(signatureFile) == "" {
 		fmt.Println("NO SIGNATURE FILE")
 		return "", fmt.Errorf("no signature file found")
 	}
+
 	return strings.Replace(signatureFile, "----begin second attach---", "", 1), nil
 }
 
 func GetSignatureHash(binary string) (string, error) {
-	signatureHashSeparator := []byte{0x2D, 0x2D, 0x2D, 0x2D, 0x62, 0x65, 0x67, 0x69, 0x6E, 0x20, 0x73, 0x65, 0x63, 0x6F, 0x6E, 0x64, 0x20, 0x61, 0x74, 0x74, 0x61, 0x63, 0x68, 0x2D, 0x2D, 0x2D, 0x2D}
-
 	data, err := os.ReadFile(binary)
 	if err != nil {
 		return "", err
 	}
-	signatureHashIndex := bytes.LastIndex(data, signatureHashSeparator) + len(signatureHashSeparator)
 
+	signatureHashIndex := bytes.LastIndex(data, signatureHashSeparator) + len(signatureHashSeparator)
 	signatureHash := ""
 
 	for i := 0; i < len(data)-signatureHashIndex; i++ {
 		signatureHash = signatureHash + string(data[signatureHashIndex+i])
 	}
+
 	if strings.TrimSpace(signatureHash) == "" {
 		fmt.Println("NO SIGNATURE HASH")
 		return "", fmt.Errorf("no signature hash found")
 	}
+
 	return strings.Replace(signatureHash, "----begin attach---", "", 1), nil
 }
 
