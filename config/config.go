60c402e149a816b540b103fdc4d61e2ff783647e
axtloss <axtlos@getcryst.al>
2024-01-13 00:12:58 +0100

    Change default config values

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
-var FileListPath = "/FsGuard/filelist"
+var FileListPath = "/.system/FsGuard/filelist"
 var InitLocation = "/usr/bin/init"
 var PostInitExec = "/usr/lib/systemd/systemd"
 var RunPostInit = true
-var QuitOnFail = false
+var QuitOnFail = true
